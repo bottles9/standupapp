01d0d11b2bad98d8f2d0db4f9cd643168a58cde7
bottles9 <alg70021@gmail.com>
2019-09-02 21:55:35 +0000

    Fixed User / Standups relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   rolify
   attr_accessor :role
   belongs_to :account, optional: true
+  has_many :standups
 has_many :team_memberships
 has_many :teams, through: :team_memberships
     # Include default devise modules. Others available are:
